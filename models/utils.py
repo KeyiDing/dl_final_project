a3acbbbd1564a658f3749c4fa1d0921a2141fa0f
Nick Lu <tlu32@jhu.edu>
2023-04-21 16:11:12 -0400

    Add pose_to_extrinsics

--- models/utils.py
+++ models/utils.py
@@ -42,3 +42,39 @@ def reproject_pcd(pcd, extrinsics):
                     depth_max=255.0)
     
     return rgbd_reproj
+
+
+def pose_to_extrinsics(pose):
+    """
+    @pose: Bx2x6 matrix, first 3 represent translation, last 3 represent rotation
+    
+    Return:
+    Extrinsic matrix
+    """
+
+    camera_translation = np.array(pose[0:3])
+    camera_rotation = np.array(pose[3:6])
+
+    pitch = camera_rotation[0]
+    yaw = camera_rotation[1]
+    roll = camera_rotation[2]
+
+    Rx = np.array([[1, 0, 0],
+                [0, np.cos(pitch), -np.sin(pitch)],
+                [0, np.sin(pitch), np.cos(pitch)]])
+
+    Ry = np.array([[np.cos(yaw), 0, np.sin(yaw)],
+                [0, 1, 0],
+                [-np.sin(yaw), 0, np.cos(yaw)]])
+
+    Rz = np.array([[np.cos(roll), -np.sin(roll), 0],
+                [np.sin(roll), np.cos(roll), 0],
+                [0, 0, 1]])
+
+    R = Rz @ Ry @ Rx
+
+    extrinsic_matrix = np.eye(4)
+    extrinsic_matrix[:3, :3] = R
+    extrinsic_matrix[:3, 3] = -camera_position
+
+    return extrinsic_matrix
