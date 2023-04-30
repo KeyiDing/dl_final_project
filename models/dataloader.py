13e0526f3481260c5555e7f6b740fb4f91461600
tli79 <tli79@jhu.edu>
2023-04-30 03:21:16 -0400

    fixed bug
    should be properly working

--- models/dataloader.py
+++ models/dataloader.py
@@ -6,21 +6,25 @@ from torch.utils.data import Dataset
 from skimage import io
 
 class DepthDataset(Dataset):
-    def __init__(self, input_dir, op, transforms=None):
-        self.transforms = transforms
-        self.op = op
-        try:
-            if self.op == 'train':
-                self.data_dir = os.path.join(input_dir, 'train')
-            elif self.op == 'val':
-                self.data_dir = os.path.join(input_dir, 'validation')
-            elif self.op == 'test':
-                self.data_dir = os.path.join(input_dir, 'test')
-        except ValueError:
-            print('op should be either train, val or test!')
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.images = os.listdir(root_dir)
+    # def __init__(self, input_dir, op, transforms=None):
+    #     self.transforms = transforms
+    #     self.op = op
+    #     try:
+    #         if self.op == 'train':
+    #             self.data_dir = os.path.join(input_dir, 'train')
+    #         elif self.op == 'val':
+    #             self.data_dir = os.path.join(input_dir, 'validation')
+    #         elif self.op == 'test':
+    #             self.data_dir = os.path.join(input_dir, 'test')
+    #     except ValueError:
+    #         print('op should be either train, val or test!')
         
     def __len__(self):
-        return len(next(os.walk(self.data_dir))[1])
+        return len(self.images) // 3
+        # return len(next(os.walk(self.data_dir))[1])
     
     def __getitem__(self, idx):
         left = Image.open(os.path.join(self.root_dir, self.images[idx * 3]))
@@ -32,7 +36,7 @@ class DepthDataset(Dataset):
         #img = io.imread(os.path.join(self.data_dir, str(idx), img_name))
         #return img
     
-    def img_transform(self, img):
-        img = self.transforms(img)
-        return img
+    # def img_transform(self, img):
+    #     img = self.transforms(img)
+    #     return img
     
